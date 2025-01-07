f0957ba1b1c0b58bea984ca4cae231003198b825
rcortese <23123652+rcortese@users.noreply.github.com>
2025-01-07 15:55:38 -0300

    actually working now

--- start_vm_shared_gpu_docker.sh
+++ start_vm_shared_gpu_docker.sh
@@ -4,35 +4,40 @@
 VM_NAME="nome_da_vm"
 CONTAINER_NAME="nome_do_container"
 
-# Verificar se a GPU está disponível
-gpu_available=$(nvidia-smi --query-gpu=memory.used --format='csv' | tail -n 1)
+is_gpu_unused() {
+  local result=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits)
+  [ "$result" = "0" ]
+}
 
-while [ "$gpu_available" = "0" ]; do
+is_container_running() {
+  local container_status
+  container_status=$(docker inspect --format '{{ .State.Running }}' "$CONTAINER_NAME" 2> /dev/null)
+  [ "$container_status" = "true" ]
+}
 
+main() {
+
+  # Parar o container
   docker stop $CONTAINER_NAME
-  
-  while [ "$(docker inspect --format '{{ .State.Running }}' "$CONTAINER_NAME" 2> /dev/null)" = "true" ]; do
+
+  # Aguardar até que o container seja desligado
+  while is_container_running; do
     sleep 1
   done
-  
-  # Iniciar a VM quando o container estiver desligado
-  virsh start $VM_NAME
-  
-  # Aguardar até que a VM esteja rodando corretamente
-  sleep 30
-  
-  # Verificar se a GPU está disponível novamente após a VM estar em execução
-  gpu_available=$(nvidia-smi --query-gpu=memory.used --format='csv' | tail -n 1)
-  
-  if [ "$gpu_available" = "0" ]; then
-    echo "GPU não está disponível"
-  else
-    echo "VM e container estão funcionando corretamente"
-  fi
-  
-  # Aguardar até que a VM esteja rodando corretamente antes de reiniciar o container
-  sleep 30
-done
 
-# Escrever os resultados para um arquivo de log
-echo "$(date) - GPU disponível: $gpu_available" >> /var/log/gpu-vm.log
+  # Iniciar a VM e aguardar até que ela esteja rodando
+  virsh start $VM_NAME
+  sleep 30
+
+  while ! is_gpu_unused; do
+    
+    echo "GPU em uso"
+    # aguardar até proxima verificacao
+    sleep 60
+  done
+  
+  # Reiniciar o container
+  docker start $CONTAINER_NAME
+}
+
+main
